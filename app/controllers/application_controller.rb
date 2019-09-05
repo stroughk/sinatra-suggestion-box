66ecbe9f617d67cbcc322c5ab0acc6531cf52249
stroughk <katerynastrough@hotmail.com>
2019-09-04 19:29:01 -0500

    add session in the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions #have access to a hash called sessions
+    set :session_secret, "ninja_secret_app"  #session id will be created for the particular session.
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,13 @@ class UsersController < ApplicationController
         erb :login 
     end
 
-    post '/login' do 
-        binding.pry 
+    post '/login' do    #the purpose of this route is to receive the login form, find the user, log the user in
+     @user = User.find_by(email: params[:email])  #to find the user
+     if @user.authenticate(params[:password]) #to authenticate the user - if user is valid, create a session
+                                                
+     else
+         
+     end
     end
 
     get '/signup' do 
