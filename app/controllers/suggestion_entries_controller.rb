19dc7d8ae130ac3d68853881c69cd3d132c393ce
stroughk <katerynastrough@hotmail.com>
2019-09-07 15:24:13 -0500

    add new, index and show files in new suggestion_entries folder in views

--- app/controllers/suggestion_entries_controller.rb
+++ app/controllers/suggestion_entries_controller.rb
@@ -1,7 +1,15 @@
 class SuggestionEntriesController < ApplicationController
 
-    get '/hotdogs' do
-        "ice cream"
+   #get suggestion_entries/new to render a form to create new entry
+
+    get '/suggestion_entries/new' do 
+        erb :'/suggestion_entries/new'
     end
+
+   # post suggestion_entries to crete a new suggestion entry
+
+   #show page for a suggestion entry
+
+   #index route for all suggestion entries 
 
 end
